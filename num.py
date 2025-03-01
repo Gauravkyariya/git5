634a7a98be71ce68d4a95bc88cb7abdfbdfb69e0
gaurav kyariya <gauravkyaiya07@gmail.com>
2025-03-01 13:05:12 +0530

    changes done

--- num.py
+++ num.py
@@ -1,7 +1,7 @@
 # Program to check if a number is even or odd
 
 # Get user input
-num = int(input("Enter a number: "))
+num = double(input("Enter a number: "))
 
 # Check if even or odd
 if num % 2 == 0:
